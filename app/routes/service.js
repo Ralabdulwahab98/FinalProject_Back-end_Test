fa20477ebe9547b2da2a6bce10563e83efed59e3
vex-xcc <vex.xcc@gmail.com>
2020-04-04 14:36:43 +0300

    commit  changes

--- app/routes/service.js
+++ app/routes/service.js
@@ -7,14 +7,14 @@ router.post('/api/:customerId', (req, res) => {
   // store new Service in memory with data from request body
   const { ServiceType, ServiceDescription, ServiceState } = req.body
   let service = {}
-    service.ServiceDescription = ServiceDescription,
+  service.ServiceDescription = ServiceDescription,
     service.ServiceState = ServiceState,
     service.RequestService = req.params.customerId,
     service.ServiceType = ServiceType
   const savedService = new Service(service)
   savedService.save()
   // find Customer in db by id and add new service
- Customer.findById(req.params.customerId, async (error, foundCustomer) => {
+  Customer.findById(req.params.customerId, async (error, foundCustomer) => {
     try {
       await foundCustomer.RequestService.push(savedService);
       foundCustomer.save()
@@ -30,30 +30,25 @@ router.post('/api/:customerId', (req, res) => {
 //------------- Find all service depend on WorkerId and if the ServiceState is open -------------\\
 router.get('/api/service/:WorkerId', (req, res) => {
   Customer.findById(req.params.WorkerId, (error, foundWorker) => {
-       let user_type = foundWorker.UserType
+    let user_type = foundWorker.UserType
     Service.find({})
-    .where('ServiceType').in(user_type)
-    .where("ServiceState").in('open')
-  .populate('ServicesEmp')
-  .exec((err, Customer) => {
-    if (err) {
-      res.status(500).send(err);
-      return;
-    }
-    console.log(`found and populated all : ${Customer}`);
-    res.status(200).json(Customer);
-  })
-
-
-// }else{
-//   console.log("there is no requested Services match you ")
-// }
-});
+      .where('ServiceType').in(user_type)
+      .where("ServiceState").in('Open')
+      .populate('ServicesEmp')
+      .exec((err, Customer) => {
+        if (err) {
+          res.status(500).send(err);
+          return;
+        }
+        console.log(`found and populated all : ${Customer}`);
+        res.status(200).json(Customer);
+      })
+  });
 
 });
 //-------------Pass service to another Customer-------------------
 router.patch('/api/PassService/:ServiceId', (req, res) => {
-    Service.findById(req.params.ServiceId, async (error, foundService) => {
+  Service.findById(req.params.ServiceId, async (error, foundService) => {
     try {
       await foundService.ServicesEmp.push(req.body.ServicesEmp);
     } catch (error) {
@@ -87,12 +82,14 @@ router.delete('/api/DeleteService/:ServiceId', (req, res) => {
   Service.findById(req.params.ServiceId, async (error, foundService) => {
     try {
       await foundService.remove();
-      res.status(200).json( `Service Id:  ${req.params.ServiceId} has been deleted `);
+      res.status(200).json(`Service Id:  ${req.params.ServiceId} has been deleted `);
     } catch (error) {
-      res.status(404).json({ error:{
-        name: 'DocumentNotFound',
-        massage:'The provided ID dose not match any Document on Service'
-    } });
+      res.status(404).json({
+        error: {
+          name: 'DocumentNotFound',
+          massage: 'The provided ID dose not match any Document on Service'
+        }
+      });
     }
   });
 });
